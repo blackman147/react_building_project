cdd06a8e57440eb83094739bfe81082f00d1967b
blackman147 <chuksntoka@gmail.com>
2021-09-23 14:09:57 +0100

    completed with redux

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import House from './components/House'
 import Balance from './components/Balance'
 import axios from 'axios'
+import { updatePillars} from "./redux/actions/action"
+import { useDispatch } from 'react-redux'
 
 const App = () => {
   const [balance, setBalance] = useState(1000000)
   const [loading, setLoading] = useState(true)
   const [pillar, setPillar] = useState(0)
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
    axios.get('http://localhost:5000/pillar')
@@ -16,6 +20,7 @@ const App = () => {
         let pillarPrice = response.data.price
         let pillarAmount = balance / pillarPrice
         setPillar(pillarAmount)
+        dispatch(updatePillars(pillarAmount))
       }).
       catch((error) => console.error(error)).
       finally(() => setLoading(false))
